6869285dfd692b8d3ed9e8c7c7bd94289059e9a9
jacksoncong517-beep <jacksoncong517@gmail.com>
2026-03-09 14:54:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def llm(prompt):
     try:
 
         response = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama-3.1-8b-instant",
             messages=[
                 {"role": "user", "content": prompt}
             ]
